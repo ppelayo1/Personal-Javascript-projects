0bf8745c55b60c752744c8dee3c9ad9b1d261676
ppelayo1 <32974267+ppelayo1@users.noreply.github.com>
2018-02-23 21:46:22 -0800

    ...

--- Just experimenting with classes/JS/java.js
+++ Just experimenting with classes/JS/java.js
@@ -1,36 +1,23 @@
 
-var pro = function () {
-    this.a = "a";
-    this.b = "b";    
-    this.c = "l";
-    this.cowboy();
-    
-    this.cowGirl = function () {
-        this.c = "yeeha";
+
+function Chil() {
+    var a = "HI";
+
+
+}
+
+function proto() {
+    var a = function () {
+        $("#first").text("Changed Text torrfafa this");
     };
-};
 
-pro.prototype.cow = "moo";
-
-pro.prototype.cowboy = function () {
-    this.milk = "got milk";
-};
+    Chil.prototype.change = a;
+}
 
 
-
-
-
-(function () {
+$().ready(function () {
+    proto();
+    var a = new Chil();
     
-    var test = new pro();
-    var test2 = new pro();
-    test2.cow = "bark";
-    test2.cow = test2.milk;
-
-    document.getElementById("test").innerHTML = test.cow;
-    document.getElementById("test2").innerHTML = test2.cow;
-    
-    
-    
-    
-})()
+    a.change();
+});
